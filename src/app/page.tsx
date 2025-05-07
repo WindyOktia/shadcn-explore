1fe04e8f71da7d205f4e0500985d535d8f540dfd
Windy Oktiagraha <win.oktia@gmail.com>
2025-05-07 16:25:37 +0700

    remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button"
-import { CirclePlus } from "lucide-react"
-
 const Homepage = ()=>{
   return (
     <div className='h-screen flex items-center justify-center'>
